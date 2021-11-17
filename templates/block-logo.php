b4ea00efe313d7a3eaa9fb0021742427ab28570f
Dan White <dan.white1@wsu.edu>
2021-11-16 16:20:51 -0800

    Fix php opening tag

--- templates/block-logo.php
+++ templates/block-logo.php
@@ -3,11 +3,11 @@ if ( true === $attributes['is_valid'] ) {
 	echo $content;
 } else {
 	$email = $attributes['author']->data->user_email;
-?>
+	?>
 	<p>
 		<b>Access Denied:</b> Missing or invalid authenication key.
 		Please contact <a href="mailto:<?php echo esc_html( $email ) ?>"><?php echo esc_html( $email ) ?></a> for assistance.
 	</p>
-	<?
+	<?php
 }
 ?>
